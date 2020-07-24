b0e8521e8cf9e6134f31c01a91e0058b2f1d3595
Paul Fischer <paul.fischer@mailbox.org>
2020-07-24 15:10:59 +0200

    added function to manually generate orders and covers in admin-interface

--- core/admin.py
+++ core/admin.py
@@ -5,11 +5,31 @@ from django.core.mail import mail_admins
 from django.template.defaultfilters import safe
 from django.template.loader import render_to_string
 from django.utils import translation
+from django.utils.text import capfirst
 from django.utils.translation import gettext_lazy as _
 
+from core.models import LectureNote
+from core.models.products import generate_cover
+from core.models.products import generate_order
 from skriptentool import settings
 
 admin.site.unregister(Group)
+
+
+def create_order(modeladmin, request, queryset):
+    for lecturenote in queryset:
+        generate_order(lecturenote)
+
+
+create_order.short_description = capfirst(_("generate orders"))
+
+
+def create_cover(modeladmin, request, queryset):
+    for lecturenote in queryset:
+        generate_cover(lecturenote)
+
+
+create_cover.short_description = capfirst(_("generate covers"))
 
 
 class Admin(admin.ModelAdmin):
@@ -59,5 +79,12 @@ class Admin(admin.ModelAdmin):
             )
 
 
+class LectureNoteAdmin(Admin):
+    actions = [create_order, create_cover]
+
+
 for model in apps.get_app_config("core").get_models():
-    admin.site.register(model, Admin)
+    if model == LectureNote:
+        admin.site.register(model, LectureNoteAdmin)
+    else:
+        admin.site.register(model, Admin)
